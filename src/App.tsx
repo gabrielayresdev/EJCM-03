311dcd8772b9ff0aa006449e6d329cf342e06ad8
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2024-01-19 15:03:30 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Titulo = styled.h1`
+  font-size: 2rem;
+  color: tomato;
+`;
+
+function App() {
+  return <Titulo>Hello World</Titulo>;
+}
+
+export default App;
